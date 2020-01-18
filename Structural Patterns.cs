9817fc7a128531f9f28a844d1f2c5e8b17f7e197
unknown <leandromoh@hotmail.com>
2020-01-18 14:48:29 -0300

    adds bridge and composite

--- Structural Patterns.cs
+++ Structural Patterns.cs
@@ -15,6 +15,25 @@ Bridge: Decouple an abstraction from its implementation so that the two can vary
 https://blog.usejournal.com/design-patterns-a-quick-guide-to-bridge-pattern-9ebf6a77baed
 https://fpierin.wordpress.com/2011/08/02/explorando-os-beneficios-do-uso-do-design-pattern-bridge/
 
+void Main()
+{
+    MoveLogic crawl = new Crawl();
+    MoveLogic walk = new Walk();
+    MoveLogic swim  = new Swim();
+
+    Animal fish = new Fish(swim);
+    fish.Move(); // swimming
+
+    Animal person = new Person(crawl);
+    person.Move(); // crawling
+
+    person.MoveLogic = walk;
+    person.Move(); // walking
+
+    person.MoveLogic = swim;
+    person.Move(); // swimming
+}
+
 Facade: Provide a unified interface to a set of interfaces in a subsystem. Façade defines a higher-level interface that makes the subsystem easier to use.
 
   class Mortgage
@@ -39,7 +58,7 @@ Decorator: Attach additional responsibilities to an object dynamically. Decorato
 
     class UserRepositoryLogTimeDecorator : AbstractUserRepositoryDecoretor
     {
-        public UserRepositoryLogDecorator(IUserRepository repository) : base(repository) { }
+        public UserRepositoryLogTimeDecorator(IUserRepository repository) : base(repository) { }
         
         public override User GetById(int id)
         {
@@ -59,3 +78,43 @@ Decorator: Attach additional responsibilities to an object dynamically. Decorato
 
 
 Proxy: Provide a surrogate or placeholder for another object to control access to it.
+
+Composite: Compose objects into tree structures to represent part-whole hierarchies. Composite lets clients treat individual objects and compositions of objects uniformly.
+//https://robsoncastilho.com.br/2013/07/10/design-patterns-usando-composite-para-montar-uma-estrutura-em-arvore/
+//https://www.dofactory.com/net/composite-design-pattern
+
+public interface ICompanyMember
+{
+	decimal CalculateCost();
+}
+
+public class Departament : ICompanyMember
+{
+	private List<ICompanyMember> _members = new List<ICompanyMember>();
+	public decimal CalculateCost() => _members.Sum(x => x.CalculateCost());
+	public void Add(ICompanyMember member) => _members.Add(member);
+	public void Remove(ICompanyMember member) => _members.Remove(member);
+}
+
+public class Employee :  ICompanyMember
+{
+	public decimal Salary;
+	public decimal CalculateCost() => Salary;
+}
+
+void Main()
+{
+	var departamentX = new Departament();
+	var departamentY = new Departament();
+
+	var employeeA = new Employee() { Salary = 1 };
+	var employeeB = new Employee() { Salary = 2 };
+	var employeeC = new Employee() { Salary = 3 };
+
+	departamentX.Add(departamentY);
+	departamentX.Add(employeeA);
+
+	departamentY.Add(employeeB);
+	departamentY.Add(employeeC);
+	departamentX.CalculateCost(); // outputs 6
+}
